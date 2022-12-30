5c2f2767325968280132ea75877cba2265cfeaec
Lee Se Eun <56140032+seeun98@users.noreply.github.com>
2022-12-30 12:08:49 +0900

    add .classpath ignore

--- src/main/java/com/hyundai/domain/Criteria.java
+++ src/main/java/com/hyundai/domain/Criteria.java
@@ -1,5 +1,7 @@
 package com.hyundai.domain;
 
-public class Criteria {
+//넘겨질 파라미터(페이지 번호, 한 페이지당 몇 개의 데이터)
 
+public class Criteria {
+	private int pageNun
 }
--- src/main/java/com/hyundai/domain/PageDTO.java
+++ src/main/java/com/hyundai/domain/PageDTO.java
@@ -1,5 +1,10 @@
 package com.hyundai.domain;
 
 public class PageDTO {
-
+	private int startPage;
+	private int endPage;
+	private boolean prev, next; // 이전, 다음 페이지 있는지 F/T
+	private int total;
+	private Criteria cri;
+	
 }
