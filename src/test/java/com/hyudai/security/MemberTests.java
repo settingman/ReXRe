d8aeb0f7dfad3c2bd4f9ca8bbd95ad02da5afd83
ILWOOCHO <earthisondanger@gmail.com>
2023-01-16 07:29:15 +0900

    testadd

--- src/test/java/com/hyudai/security/MemberTests.java
+++ src/test/java/com/hyudai/security/MemberTests.java
@@ -1,5 +1,7 @@
 package com.hyudai.security;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.junit.Test;
@@ -10,6 +12,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.hyundai.domain.AllergyMapperVO;
+import com.hyundai.domain.AllergyVO;
+import com.hyundai.domain.MemberVO;
 import com.hyundai.mapper.MemberMapper;
 
 import lombok.Setter;
@@ -119,31 +123,126 @@ public class MemberTests {
 //		}
 //	}
 	@Test
+	public void testRead() {
+		System.out.println(mapper.read("joilwoo"));
+	}
+	@Test
+	public void testIDCheck() {
+		System.out.println(mapper.IDCheck("joilwoo"));
+	}
+	@Test
+	public void allergySearchTest(){
+		List<AllergyVO> list = mapper.allergySearch("소듐");
+		for(AllergyVO vo : list) {
+			System.out.println(vo);
+		}
+	}
+	@Test
+	public void addallergytest() {
+		mapper.addal("낙옆 추출물");
+	}
+	@Test
+	public void joinUserTest() {
+		MemberVO vo = new MemberVO();
+		vo.setEnabled(1);
+		vo.setMember_address1("서울시");
+		vo.setMember_address2("동작구");
+		vo.setMember_birthday("1995-06-28");
+		vo.setMember_email("hi@hi.hi");
+		vo.setMember_phone("01020392939");
+		vo.setMember_postNum("11111");
+		vo.setMember_sex(1);
+		vo.setUseridid("hihi1231");
+		vo.setUserName("공");
+		PasswordEncoder pw = new PasswordEncoder() {
+			public String encode(CharSequence rawPassword) {
+				return rawPassword.toString();
+			}
+
+			@Override
+			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+				
+				return rawPassword.toString().equals(encodedPassword);
+			}
+		};
+		vo.setUserpw(pw.encode("123123123"));
+		mapper.joinUser(vo);
+	}
+	@Test
+	public void joinUserAuthTest() {
+		mapper.joinUserAuth("hihi1231");
+	}
+	@Test
+	public void addMemAlTest() {
+		AllergyMapperVO vo = new AllergyMapperVO();
+		vo.setMember_idid("hihi1231");
+		vo.setAllergy_name("Test");
+		mapper.addMemAl(vo);
+	}
+	@Test
+	public void findIDTest() {
+		System.out.println(mapper.findID("공", "hi@hi.hi"));
+	}
+	@Test
+	public void PWCheckTest() {
+		System.out.println(mapper.PWCheck("hihi1231"));
+	}
+	@Test
+	public void updateUserTest() {
+		MemberVO vo = new MemberVO();
+		vo.setEnabled(1);
+		vo.setMember_address1("청주시");
+		vo.setMember_address2("동작구");
+		vo.setMember_birthday("1995-06-28");
+		vo.setMember_email("hi@hi.hi");
+		vo.setMember_phone("01020392939");
+		vo.setMember_postNum("11111");
+		vo.setMember_sex(1);
+		vo.setUseridid("hihi1231");
+		vo.setUserName("공");
+		PasswordEncoder pw = new PasswordEncoder() {
+			public String encode(CharSequence rawPassword) {
+				return rawPassword.toString();
+			}
+
+			@Override
+			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+				
+				return rawPassword.toString().equals(encodedPassword);
+			}
+		};
+		vo.setUserpw(pw.encode("123123123"));
+		 mapper.updateUser(vo);
+	}
+
+	@Test
 	public void testPWChange() {
 		mapper.pwChange("joilwoo", pwencoder.encode("12312312"));
 	}
 	
-	@Test
-	public void read() {
-		System.out.println(mapper.read("123123123"));
-	}
-	
-	@Test
-	public void joinUserAuth123() {
-		
-		mapper.joinUserAuth("joilwoo");
-	}
 	@Test
 	public void findMemAltest() {
 		System.out.println(mapper.findMemAl("member1"));
 		
 	}
 	@Test
+	public void ididTest() {
+		System.out.println(mapper.idid("hihi1231"));
+	}
+	@Test
+	public void findPWTest() {
+		System.out.println(mapper.findPW("hihi1231", "공", "hi@hi.hi"));
+	}
+	@Test
 	public void deleteMemAltest() {
 		AllergyMapperVO vo = new AllergyMapperVO();
-		vo.setAllergy_name("나트륨");
-		vo.setMember_idid("member1");
+		vo.setAllergy_name("소듐");
+		vo.setMember_idid("hihi1231");
 		mapper.deleteMemAl(vo);
 		
+	}
+	@Test
+	public void outTest() {
+		mapper.out("hihi1231");
 	}
 }
